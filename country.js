e4cf1901ba27f49a5f856c7a547ec0bb026ab03f
Tino Di Costanzo <tino.dicostanzo90@gmail.com>
2023-11-28 18:31:09 +0100

    add style.css - index.html - country.js

---
+++ country.js
@@ -0,0 +1,9 @@
+const country = "Italy";
+const continent = "Europe";
+let population = 59.11;
+const isIsland = false;
+let languege =
+
+    console.log(population + " " + isIsland + " " + country);
+
+isIsland = true;
