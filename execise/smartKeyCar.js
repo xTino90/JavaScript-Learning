487f02d543f0041d5ca39f4d86f6dcccce6edf1f
Tino Di Costanzo <tino.dicostanzo90@gmail.com>
2023-12-05 06:36:03 +0100

    add object

--- execise/smartKeyCar.js
+++ execise/smartKeyCar.js
@@ -1,1 +1,26 @@
 // Smar key car, work only for over 18 years old
+
+const jonas = {
+
+};
+
+// jonas.fistName = "jonas";
+// jonas.lastName = "something";
+// jonas.age = 2047 - 1991;
+// jonas.friends = ["Michael", "Pep", "Giuvan"];
+
+
+jonas["firstName"] = "jonas";
+jonas["lastName"] = "something";
+jonas["age"] = 2047 - 1991;
+jonas["driveLicense"] = false;
+console.log(jonas);
+
+jonas["saluta"] = function () {
+    this.saluta = "hello everyone!!";
+    return this.saluta;
+}
+
+console.log(jonas.saluta());
+
+console.log(`${jonas.firstName} has ${jonas.age}-years old, and he has ${(jonas.driveLicense ? "a" : "no")} diriver's license. He says ${jonas.saluta}`);
