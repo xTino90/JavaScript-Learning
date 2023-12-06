61639b576d6d6a9250e5d1fed60846c155865a45
Tino Di Costanzo <tino.dicostanzo90@gmail.com>
2023-12-06 23:02:13 +0100

    fix bmi

--- execise/BMI.js
+++ execise/BMI.js
@@ -1,26 +1,46 @@
-const markHeight = 1.69;
-const johnHeight = 1.95;
-const markMass = 78;
-const johnMass = 95;
+// const markHeight = 1.69;
+// const johnHeight = 1.95;
+// const markMass = 78;
+// const johnMass = 95;
 
-let bmiMark = 78 / (1.68 ** 2);
-console.log(bmiMark);            // 27.636054421768712
+// let bmiMark = 78 / 1.68 ** 2;
+// console.log(bmiMark); // 27.636054421768712
 
-let bmiJohn = 95 / (1.95 ** 2);
-console.log(bmiJohn);           //24.983563445101908
+// let bmiJohn = 95 / 1.95 ** 2;
+// console.log(bmiJohn); //24.983563445101908
 
-markHeightBmi = bmiMark > bmiJohn;
-console.log(markHeightBmi);      // true
+// markHeightBmi = bmiMark > bmiJohn;
+// console.log(markHeightBmi); // true
 
+// if (bmiMark > bmiJohn) {
+//   console.log(`MarkBMI (${bmiMark}) is higher than JohnBMI (${bmiJohn})`);
+// } else {
+//   console.log(` JohnBMI(${bmiJohn}) is higher than MarkBMI (${bmiMark})`);
+// }
 
-if (bmiMark > bmiJohn) {
-    console.log(`MarkBMI (${bmiMark}) is higher than JohnBMI (${bmiJohn})`);
-} else {
-    console.log(` JohnBMI(${bmiJohn}) is higher than MarkBMI (${bmiMark})`);
+const john = {
+  name: "John",
+  lasName: "Smith",
+  height: "1,69",
+  mass: "78",
+  calcBMI: function (mass, height) {
+    return mass / height ** 2;
+  },
+};
+john["bmi"] = john.calcBMI(78, 1.69).toFixed(2);
+
+const mark = {
+  name: "Mark",
+  lasName: "Miller",
+  height: "1,95",
+  mass: "92",
+  calcBMI: function (mass, height) {
+    return mass / height ** 2;
+  },
 };
 
+mark["bmi"] = mark.calcBMI(95, 1.95).toFixed(2);
 
-
-
-
-
+console.log(
+  `${john.name} ${john.lasName}'s BMI(${john.bmi}) is higher than ${mark.name} ${mark.lasName}'s BMI(${mark.bmi})!`
+);
--- execise/switch-statement.js
+++ execise/switch-statement.js
@@ -1,21 +1,27 @@
 const bill = 430;
 let tip;
 if (bill >= 50 && bill <= 300) {
-    tip = 15
-    // console.log("tip is 15%");
+  tip = 15;
+  // console.log("tip is 15%");
 } else {
-    tip = 20
-    // console.log("tip is 20%");
+  tip = 20;
+  // console.log("tip is 20%");
 }
 let billTip = (bill * tip) / 100;
 let totalPay = bill + billTip;
 
-console.log(`The bill was ${bill}, the tip was ${billTip}, and the total value ${totalPay}`);
+console.log(
+  `The bill was ${bill}, the tip was ${billTip}, and the total value ${totalPay}`
+);
 
 //-------------------------------TERNARY OPERETOR------------------------//
 
 const bill = 430;
 
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-console.log(`The bill was ${bill}$, the tip was ${tip}$, and the total value ${bill + tip}$`);
+console.log(
+  `The bill was ${bill}$, the tip was ${tip}$, and the total value ${
+    bill + tip
+  }`
+);
