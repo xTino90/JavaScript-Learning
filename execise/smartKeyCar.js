89caeb84b7ece34066ed875a8bbc0de5a33c2299
Tino Di Costanzo <tino.dicostanzo90@gmail.com>
2023-12-06 05:54:04 +0100

    fix

--- execise/smartKeyCar.js
+++ execise/smartKeyCar.js
@@ -12,15 +12,22 @@ const jonas = {
 
 jonas["firstName"] = "jonas";
 jonas["lastName"] = "something";
-jonas["age"] = 2047 - 1991;
+jonas["birthday"] = 1991;
 jonas["driveLicense"] = false;
 console.log(jonas);
 
+jonas["calcAge"] = function () {
+    this.Age = 2046 - this.birthday;
+    return this.Age;
+};
 jonas["saluta"] = function () {
     this.saluta = "hello everyone!!";
     return this.saluta;
-}
+};
 
+jonas["getSummary"] = function () {
+    return `${this.firstName} has ${this.calcAge()}-years old, and he has ${(this.driveLicense ? "a" : "no")} diriver's license. He says ${this.saluta}`;
+};
 console.log(jonas.saluta());
 
-console.log(`${jonas.firstName} has ${jonas.age}-years old, and he has ${(jonas.driveLicense ? "a" : "no")} diriver's license. He says ${jonas.saluta}`);
+console.log(jonas.getSummary());
