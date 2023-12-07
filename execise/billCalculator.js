a782a6d4a6fca28a1fa2aa76e7bf1cba41a1aae7
Tino Di Costanzo <tino.dicostanzo90@gmail.com>
2023-12-07 23:08:34 +0100

    new exercise add

--- execise/billCalculator.js
+++ execise/billCalculator.js
@@ -1,29 +1,27 @@
-// Steven is still building his tip calculator, using the same rules as before: Tip 15% of 
-// the bill if the bill value is between 50 and 300, and if the value is different, the tip is 
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
 // 20%.
 
 // Your tasks:
-// 1. Write a function 'calcTip' that takes any bill value as an input and returns 
-// the corresponding tip, calculated based on the rules above (you can check out 
-// the code from first tip calculator challenge if you need to). Use the function 
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
 // type you like the most. Test the function using a bill value of 100
 
-// 2. And now let's use arrays! So create an array 'bills' containing the test data 
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
 // below
 
-// 3. Create an array 'tips' containing the tip value for each bill, calculated from 
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
 // the function you created before
 
 // 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
 // Test data: 125, 555 and 44
 
-// Hint: Remember that an array needs a value in each position, and that value can 
-// actually be the returned value of a function! So you can just call a function as array 
-// values (so don't store the tip values in separate variables first, but right in the new 
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
 // array)
 `strict mode`;
-
-
 
 // const calcTip = function (bill) {
 //     return bill > 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
@@ -31,14 +29,52 @@
 // }
 
 //arrow function
-calcTip = bill => bill > 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+// calcTip = bill => bill > 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 
+// const bills = [125, 555, 44];
+// const tips = [calcTip(125), calcTip(555), calcTip(44)];
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-const bills = [125, 555, 44];
-const tips = [calcTip(125), calcTip(555), calcTip(44)];
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(bills);
+// console.log(tips);
+// console.log(total);
+//___________________________________________________________________________challange!!!________
 
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
 
-console.log(bills);
-console.log(tips);
-console.log(total);
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+// tips and total values (bill + tip) for every bill value in the bills array.
+
+//Use a for loop to perform the 10 calculations!
+
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays
+
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+// 4.1. First, you will need to add up all values in the array. To do the addition,
+// start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop. In each iteration, add the current value to the
+// 'sum' variable. This way, by the end of the loop, you have all values
+// added together
+// 4.2. To calculate the average, divide the sum you calculated before by the
+// length of the array (because that's the number of elements)
+// 4.3. Call the function with the 'totals' array
+
+const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+calcTip = function (bill) {
+  return bill > 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+for (let i = 0; i < bill.lenght; i++) {
+  tips.push(calcTip(bill[i]));
+}
+console.log(calcTip(bill[]));
--- execise/country.js
+++ execise/country.js
@@ -1,9 +1,87 @@
-const country = "Italy";
-const continent = "Europe";
-let population = 59.11;
-const isIsland = false;
-let languege =
+"strict mode";
+//-------------------------------Part 1---------------------//
 
-    console.log(population + " " + isIsland + " " + country);
+// const country = "Italy";
+// const continent = "Europe";
+// let population = 60;
+// const isIsland = false;
+// const languege = "Italian";
 
+// // let halfPop = population / 2;
+// // let pop1 = population ++;
+// // let finlandPop = population > 6;
+// // let avgPop = population > 33;
 
+// const description = `${country} is in ${continent}, and its ${population} million people speak ${languege}!`;
+// console.log(description);
+
+// // if (population > 33) {
+// //   console.log(`${country}'s population is above avarage`);
+// // } else {
+// //   console.log(
+// //     `${country}'s population is ${33 - population} million below avarege`
+// //   );
+// // }
+// // console.log(
+// //   "9" - "5", //4
+// //   "19" - "13" + "17",//617
+// //   "19" - "13" + 17,//23
+// //   "123" < 57,//false
+// //   5 + 6 + "4" + 9 - 4 - 2//1143
+// // );
+
+// // const nunNeighbours = Number(
+// //   prompt("How many neighbour countries does your country have?")
+// // );
+
+// // if (nunNeighbours === 1) {
+// //   console.log("Only 1 border!");
+// // } else if (nunNeighbours > 1) {
+// //   console.log("More then 1 border");
+// // } else {
+// //
+// //   console.log("no border");
+// // }
+
+// // if (languege && population < 50 && !isIsland) {
+// //   sarah = console.log("You should live in Portugal");
+// // } else {
+// //   sarah = console.log("Italy does not meet your criteria");
+// // }
+
+// // switch (languege) {
+// //   case ("chinese", "mandarine"):
+// //     console.log("MOST number of native speakers!");
+// //     break;
+// //   case "spanish":
+// //     console.log("2nd place in number of native speakers");
+// //     break;
+// //   case "english":
+// //     console.log("3rd palce");
+// //   case "hindi":
+// //     console.log("Number 4");
+// //     break;
+// //   case "arabic":
+// //     console.log("5th most spoken language");
+// //     break;
+// //   default:
+// //     console.log("Great language too :D");
+// // }
+
+// population > 33
+//   ? console.log("Italy's population is above avarege")
+//   : console.log("Italy's population is below average");
+//-------------------------------Part 2---------------------//
+let country;
+let population;
+let capitalCity;
+
+describeCountry = function (country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital citi id ${capitalCity}`;
+};
+
+const desItaly = describeCountry("Italy", 60, "Rome");
+const desSpanish = describeCountry("Spanish", 44, "Madrid");
+const desHolland = describeCountry("Holland", 12, "Amsterdam");
+
+console.log(desItaly, desHolland, desSpanish);
